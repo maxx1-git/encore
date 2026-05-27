3e33473ae5380b3c97700d31b2da807f841ca213
max <polamaxp@gmail.com>
2026-05-27 19:45:02 +0300

    dbsqlite3 database used for this project

--- project/project/settings.py
+++ project/project/settings.py
@@ -79,9 +79,10 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 
